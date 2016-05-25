5dda6329cc02d519e69dee8de451f455c5ec56a4
서일현 <ilhyun.seo@ysmcorp.com>
2016-05-25 09:23:10 +0900

    day9, java strings solved

---
+++ README.md
@@ -0,0 +1,7 @@
+### Hacker Rank
+
+* tutorial : https://www.hackerrank.com/domains/tutorials/30-days-of-code
+
+* java :
+  - introduction : https://www.hackerrank.com/domains/java/java-introduction
+  - strings : https://www.hackerrank.com/domains/java/java-strings/difficulty/all/page/1
---
+++ src/com/hacker/rank/tutorials/day9/Solution.java
@@ -0,0 +1,30 @@
+package com.hacker.rank.tutorials.day9;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+		
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		Solution solution = new Solution();
+		int result = solution.factorial(N);
+		
+		System.out.println(result);
+		
+		in.close();
+	}
+	
+	
+	public int factorial(int n) {
+		if( n > 0) {
+			return n * factorial(n-1);
+		} else {
+			return 1;
+		}
+	}
+}
