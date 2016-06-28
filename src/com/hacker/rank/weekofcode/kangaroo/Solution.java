48b9a533ebde1b8b77edef11d54a055aa30846cd
서일현 <ilhyun.seo@ysmcorp.com>
2016-06-28 15:21:25 +0900

    luck-balance 문제 풀이

--- src/com/hacker/rank/weekofcode/kangaroo/Solution.java
+++ src/com/hacker/rank/weekofcode/kangaroo/Solution.java
@@ -6,6 +6,23 @@ import java.util.Scanner;
  * Created by enosent on 2016. 6. 28..
  */
 public class Solution {
+	
+	/*
+	 * Note: The two kangaroos must land at the same location after making the same number of jumps.
+	 * 
+	 * .. 수학 다 까먹었네 ㅡㅡ;;
+	 * 
+	 * same number of jumps!!
+	 * 
+	 * 방정식 문제였어 ㅡㅡ;
+	 * 
+	 * x1 + v1*Jump = x2 + v2*Jump
+	 *  > x1 - x2 = v2*Jump - v1*Jump
+	 *  > x1 - x2 = (v2 - v1)*Jump
+	 *  > Jump = (x1 - x2) / (v2 - v1)
+	 * 
+	 * Jump = (x1 - x2) / (v2 - v1) 이 값이 0으로 떨어지는 것이 .. YES인 ..
+	 */
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -21,14 +38,12 @@ public class Solution {
         } else if(x1 != x2 && v1 == v2){
             System.out.println("NO");
         } else{
-            while(true) {
-                x1 = (x1 + v1);
-                x2 = (x2 + v2);
-
-                if(x1 == x2) {
-                    System.out.println("YES");
-                    break;
-                }
+        	if(v2 - v1 == 0) {
+        		System.out.println("NO");	
+        	} else if( (x1-x2)%(v2-v1) == 0 ){
+        		System.out.println("YES");
+            } else {
+            	System.out.println("NO");
             }
         }
         in.close();
